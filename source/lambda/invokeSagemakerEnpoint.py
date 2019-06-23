64492e11109cdd4f72e93e2cb1ca512b164f0cc1
Gopi Krishnamurthy <gopicares@gmail.com>
2019-06-24 01:12:37 +0200

    adding invoker lambda

---
+++ source/lambda/invokeSagemakerEnpoint.py
@@ -0,0 +1,34 @@
+import os
+import io
+import boto3
+import json
+import csv
+
+# grab environment variables
+# ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
+runtime= boto3.client('runtime.sagemaker')
+endpoint_param = boto3.client('ssm')
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+    
+    data = json.loads(json.dumps(event))
+    payload = data['data']
+    print(payload)
+    
+    ssm_ep = endpoint_param.get_parameter(
+        Name='SM_PROD_ENDPOINT_NAME'
+    )
+    ENDPOINT_NAME = ssm_ep["Parameter"].get("Value")
+    print(ssm_ep["Parameter"].get("Value"))
+
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                       ContentType='text/csv',
+                                       Body=payload)
+    print(response)
+    result = json.loads(response['Body'].read().decode())
+    print(result)
+    pred = int(result['predictions'][0]['predicted_label'])
+    predicted_label = 'M' if pred == 1 else 'B'
+    
+    return predicted_label

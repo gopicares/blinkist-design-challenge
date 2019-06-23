55ebbe480802caf2594d2c60b9db602e989b5b40
Gopi Krishnamurthy <gopicares@gmail.com>
2019-06-24 00:24:30 +0200

    run unit test bash

--- tests/unit_testing.sh
+++ tests/unit_testing.sh
@@ -1,1 +1,6 @@
-
+#! /bin/bash
+aws cloudformation --region eu-central-1 create-stack --stack-name test-sagemaker-notebook \
+  --template-url https://s3.amazonaws.com/<<your bucket>>/sagemake_notebook_instance.yaml \
+  --parameters file://test-sagemaker-params.json \
+  --capabilities "CAPABILITY_IAM" \
+  --disable-rollback
